aed06a7450c343497086ed82ab942da619486f94
Alex Buccheri <alexanderbuccheri@googlemail.com>
2024-03-22 21:46:37 +0000

    Test `assign_points_to_centroids`

--- src/isdf_prototyping/interpolation_points.py
+++ src/isdf_prototyping/interpolation_points.py
@@ -3,49 +3,90 @@
 * QR decomposition with column pivoting
 * Weighted k-means clustering
 """
-
 from typing import List
 
+import numba
 import numpy as np
 from scipy.spatial.distance import cdist
 
 cluster_type = List[List[int]]
 
 
-def assign_points_to_centroids(
-    grid_points: np.ndarray, centroids: np.ndarray
-) -> cluster_type:
+def is_subgrid_on_grid(subgrid: np.ndarray, grid: np.ndarray, tol: float) -> list:
+    """Return indices of subgrid points not present in grid.
+
+    Parallelisation:
+    * Won't benefit from Numba, as only called once
+    * Loop shouldn't be large enough to parallelise with MPI
+      - One could thread it
+    * Function might benefit from cupy
+
+    :param subgrid:
+    :param grid:
+    :return:
     """
+    N_sub = subgrid.shape[0]
+    indices = []
+    for i in range(N_sub):
+        centred_on_point_i = np.linalg.norm(grid - subgrid[i], axis=1)
+        matched_indices = np.argwhere(centred_on_point_i <= tol)
+        if matched_indices.size == 0:
+            indices.append(i)
+    return indices
+
+
+def assign_points_to_centroids(grid_points: np.ndarray, centroids: np.ndarray) -> cluster_type:
+    """ Assign each grid point to the closest centroid. A centroid and its set of nearest
+    grid points defines a cluster.
+
+    Refactor:
+    * Could work on the distance matrix for all points
+    * Could simplify such that I just use argmin. Not likely that points will be equidistant
+      in all but symmetric, toy problems
+
+    Parallelisation Strategies:
+    * Could benefit from MPI distribution of loop
+    * ~Could benefit from NUMBA, as called multiple times~   cdist is scipy, not numpy
+      - Would have to @overload(scipy.spatial.distance.cdist) and supply my own compiled implementation
+    * Could benefit from cupy as *some* numpy operations are used
+
+    :param grid_points: Grid point
+    :param centroids: Centroid points
 
     :return: clusters: List of length N interpolation points. Each element corresponds to a cluster,
     which indexes all grid points associated with it
     """
     N_r, dim = grid_points.shape
     N_interp = centroids.shape[0]
-    clusters: cluster_type = [[]] * N_interp
+    clusters: cluster_type = [[] for _ in range(N_interp)]
 
     # Computing distance matrix per grid point because I want to distribute this loop
+    # (keeping a fortran implementation in mind)
     for ir in range(0, N_r):
-        distance_matrix = cdist(grid_points[ir, :], centroids)
+        # Need to retain a 2D array for cdist
+        point = grid_points[ir, :][None, :]
+        # distance matrix has shape (1, N_interp)
+        distance_matrix = cdist(point, centroids)
         # min(|r_{ir} - centroids|)
-        # TODO, this is probs a 2D index - will want the column
-        icen = np.argmin(distance_matrix)
+        # If two or more elements are equally minimal, argmin will always return the first instance
+        # Instead, we find all equally minimum indices
+        min_indices = np.argwhere(distance_matrix == distance_matrix.min())
+        # Reduce to the second column (which contains the meaningful data), and choose one index at random
+        random_index = np.random.choice(min_indices[:, 1])
+        icen = min_indices[random_index, 1]
         clusters[icen].append(ir)
-
     return clusters
 
 
-def update_centroids(
-    grid_points, f_weight, clusters: cluster_type
-) -> np.ndarray:
+def update_centroids(grid_points, f_weight, clusters: cluster_type) -> np.ndarray:
     """Compute a new set of centroids
 
-    Have as many clusters as we do centroids
+    We have as many clusters as we do centroids
 
-    :param grid_points:
-    :param f_weight:
-    :param indices:
-    :return:
+    :param grid_points: Grid
+    :param f_weight: Weight function
+    :param clusters: Grid point indices associated with each cluster
+    :return: updated_centroids: Updated centroids
     """
     N_interp = len(clusters)
     dim = grid_points.shape[1]
@@ -77,11 +118,9 @@ def points_are_converged(updated_points, points, tol, verbose=False) -> bool:
     if verbose:
         N = updated_points.shape[0]
         if converged:
-            print(f"Convergence: All points converged")
+            print("Convergence: All points converged")
         else:
-            print(
-                f"Convergence: {len(indices)} points out of {N} are not converged:"
-            )
+            print(f"Convergence: {len(indices)} points out of {N} are not converged:")
             print("# Current Point    Prior Point    |ri - r_{i-1}|   tol")
             for i in indices:
                 print(updated_points[i, :], points[i, :], norm[i], tol)
@@ -89,37 +128,19 @@ def points_are_converged(updated_points, points, tol, verbose=False) -> bool:
     return converged
 
 
-def is_subgrid_on_grid(subgrid: np.ndarray, grid: np.ndarray, tol: float) -> list:
-    """Return indices of subgrid points not present in grid.
-
-    Parallelisation:
-    * Won't benefit from Numba, as only called once
-    * Loop shouldn't be large enough to parallelise with MPI
-      - One could thread it
-    * Function might benefit from cupy
-
-    :param subgrid:
-    :param grid:
-    :return:
-    """
-    N_sub = subgrid.shape[0]
-    indices = []
-    for i in range(N_sub):
-        centred_on_point_i = np.linalg.norm(grid - subgrid[i], axis=1)
-        matched_indices = np.argwhere(centred_on_point_i <= tol)
-        if matched_indices.size == 0:
-            indices.append(i)
-    return indices
+def verbose_print(*args, verbose=True, **kwargs):
+    if verbose:
+        print(*args, **kwargs)
 
 
 def weighted_kmeans(
-    grid_points: np.ndarray,
-    f_weight: np.ndarray,
-    centroids: np.ndarray,
-    n_iter=200,
-    centroid_tol=1.0e-6,
-    safe_mode=False,
-    verbose=True,
+        grid_points: np.ndarray,
+        f_weight: np.ndarray,
+        centroids: np.ndarray,
+        n_iter=200,
+        centroid_tol=1.0e-6,
+        safe_mode=False,
+        verbose=True,
 ) -> np.ndarray:
     """
 
@@ -160,18 +181,13 @@ def weighted_kmeans(
         )
         raise ValueError(err_msg)
 
-    if verbose:
-        print("Centroid Optimisation")
-    updated_centroids = np.empty_like(centroids)
+    verbose_print("Centroid Optimisation", verbose)
 
     for t in range(0, n_iter):
         clusters = assign_points_to_centroids(grid_points, centroids)
         updated_centroids = update_centroids(grid_points, f_weight, clusters)
-        if verbose:
-            print(f"Step {t}")
-        converged = points_are_converged(
-            centroids, updated_centroids, centroid_tol, verbose=verbose
-        )
+        verbose_print(f"Step {t}", verbose)
+        converged = points_are_converged(centroids, updated_centroids, centroid_tol, verbose=verbose)
         if converged:
             return updated_centroids
         centroids = updated_centroids
--- tests/test_interpolation_points.py
+++ tests/test_interpolation_points.py
@@ -1,53 +1,95 @@
 import numpy as np
+from unittest.mock import patch
+
+from src.isdf_prototyping.interpolation_points import is_subgrid_on_grid, assign_points_to_centroids
 
 
-from src.isdf_prototyping.interpolation_points import is_subgrid_on_grid
+def construct_2d_grid(x_p, y_p) -> np.ndarray:
+    """
+
+    x and y args are the same ordering as with np.linspace
+    i.e. start, stop, num
+
+    :param x_p:
+    :param y_p:
+    :return:
+    """
+    x_values = np.linspace(*x_p)
+    y_values = np.linspace(*y_p)
+    x, y = np.meshgrid(x_values, y_values)
+    grid = np.vstack([x.ravel(), y.ravel()]).T
+    return grid
 
 
 def test_is_subgrid_on_grid():
+
     # 2D grid, using arbitrary limits
-    x_values = np.linspace(0, 10, 5)
-    y_values = np.linspace(0, 1, 5)
-    x, y = np.meshgrid(x_values, y_values)
-    grid = np.vstack([x.ravel(), y.ravel()]).T
+    grid = construct_2d_grid((0, 10, 5), (0, 1, 5))
     assert grid.shape == (25, 2), "N grid points by M dimensions"
 
     subgrid = grid[2:4, :]
-    indices = is_subgrid_on_grid(subgrid, grid, tol=1.e-6)
+    indices = is_subgrid_on_grid(subgrid, grid, tol=1.0e-6)
     assert not indices, "All subgrid points should be defined on the grid"
 
     # All elements
-    noisy_subgrid = subgrid + 1.e-5
+    noisy_subgrid = subgrid + 1.0e-5
     n_sub = noisy_subgrid.shape[0]
-    indices = is_subgrid_on_grid(noisy_subgrid, grid, tol=1.e-6)
+    indices = is_subgrid_on_grid(noisy_subgrid, grid, tol=1.0e-6)
     assert indices == [i for i in range(n_sub)], "No subgrid points should be defined on the grid"
 
     # Single element
     noisy_subgrid = np.copy(subgrid)
-    noisy_subgrid[1, :] += 1.e-5
-    indices = is_subgrid_on_grid(noisy_subgrid, grid, tol=1.e-6)
+    noisy_subgrid[1, :] += 1.0e-5
+    indices = is_subgrid_on_grid(noisy_subgrid, grid, tol=1.0e-6)
     assert indices == [1], "One subgrid point should not be on the grid"
 
 
-def test_points_are_converged():
-    pass
-
-
 def test_assign_points_to_centroids():
-    # Define a grid
-    # Define a few centroids
-    # Ensure one can do this by inspection
-    # Confirm grid points are assigned to correct clusters
+    """
+    Define a grid comprised of circular grid points
+    Place one centroid at its centre and the other away from it
+    All points should assign to the same centroid
+    """
+    centroids = np.array([[0, 0], [5, 0]])
+    radius = 2
+    n_points = 10
+    grid = np.empty(shape=(n_points, 2))
+    for i in range(0, n_points):
+        theta = (float(i + 1) / float(n_points)) * 2 * np.pi
+        grid[i, :] = centroids[0, :] + radius * np.array([np.cos(theta), np.sin(theta)])
 
-    # Do a second case where the points (choose several) are
-    # equivdistant from two centroids, and confirm which clusters
-    # each point gets assigned to - want this to be random, but will
-    # need to build that mechanism in (then monkey patch for testing)
-    pass
+    clusters = assign_points_to_centroids(grid, centroids)
+    assert clusters[0] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], "All grid points in first cluster"
+    assert clusters[1] == [], "No grid points in second cluster"
+
+
+def test_assign_points_equidistant_to_centroids():
+    """ Test assignment of points that are equidistant to two centroids
+    """
+    # Three points all equidistant between two centroids
+    grid = np.array([[0, 0], [0.5, 0.5], [1., 1.]])
+    centroids = np.array([[0., 1.], [1., 0]])
+
+    clusters = assign_points_to_centroids(grid, centroids)
+    assert set(clusters[0]) | set(clusters[1]) == {0, 1, 2}, \
+        "All points assigned randomly between the two clusters "
+
+    with patch("isdf_prototyping.interpolation_points.np.random.choice") as mock_random:
+        mock_random.return_value = 0
+        clusters = assign_points_to_centroids(grid, centroids)
+        assert clusters == [[0, 1, 2], []], 'Mock random so all equi-d points go to cluster 0'
+
+        mock_random.return_value = 1
+        clusters = assign_points_to_centroids(grid, centroids)
+        assert clusters == [[], [0, 1, 2]], 'Mock random so all equi-d points go to cluster 1'
 
 
 def test_update_centroids():
     # Can test with some trivial and specific weight functions
+    pass
+
+
+def test_points_are_converged():
     pass
 
 
